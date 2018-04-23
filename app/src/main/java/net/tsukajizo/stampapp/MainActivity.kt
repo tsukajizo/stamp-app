a1a2a62652ec3386e3794086e8876e4ed7854f03
shuzo <tsukajizo@gmail.com>
2018-04-23 20:47:32 +0900

    initialize app

---
+++ app/src/main/java/net/tsukajizo/stampapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.tsukajizo.stampapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
