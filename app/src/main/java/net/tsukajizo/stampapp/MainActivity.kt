473007e84d9f083b1a86f81b79ac6787c54a11e9
shuzo <tsukajizo@gmail.com>
2018-05-01 20:52:44 +0900

    下タブのナビゲーションを作成

--- app/src/main/java/net/tsukajizo/stampapp/MainActivity.kt
+++ app/src/main/java/net/tsukajizo/stampapp/MainActivity.kt
@@ -2,6 +2,8 @@ package net.tsukajizo.stampapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottom_navigation.setOnNavigationItemSelectedListener({
+            when (it.itemId) {
+                R.id.nav_camera -> {
+                    setFragment()
+                }
+                R.id.nav_stamp -> {
+                    setFragment()
+                }
+                R.id.nav_map -> {
+                    setFragment()
+                }
+                else -> {
+                }
+            }
+            true
+        })
+        setFragment()
+    }
+
+    private fun setFragment() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fl_fragment_container, StampViewerFragment.newInstance())
         transaction.commit()
