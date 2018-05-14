891f7425d5348a972f9ef45a03a843cb4df7abea
shuzo <tsukajizo@gmail.com>
2018-05-14 20:52:37 +0900

    メソッドの整理

--- app/src/main/java/net/tsukajizo/stampapp/presentation/viewer/StampListAdapter.kt
+++ app/src/main/java/net/tsukajizo/stampapp/presentation/viewer/StampListAdapter.kt
@@ -1,5 +1,6 @@
 package net.tsukajizo.stampapp.presentation.viewer
 
+import android.content.Context
 import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -18,38 +19,38 @@ public class StampListAdapter @Inject constructor(private val app: App) : Recycl
     var list: List<Stamp>? = null
     var itemClickListener: OnItemClickListener? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(app.applicationContext)
-        return ViewHolder(inflater.inflate(R.layout.stamp_list_item, parent, false))
-    }
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder =
+            ViewHolder(LayoutInflater.from(app.applicationContext).inflate(R.layout.stamp_list_item, parent, false))
 
 
     override fun getItemCount(): Int {
         return list?.size ?: 0
     }
 
-    override fun onViewRecycled(viewHolder: ViewHolder) {
-        Glide.with(app.applicationContext).clear(viewHolder.imageView)
-    }
+    override fun onViewRecycled(viewHolder: ViewHolder) =
+            viewHolder.recycle(app.applicationContext)
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        if (itemCount > position) {
-            val stamp = list!![position]
-            if (stamp.isGathered) {
-                val path = list?.get(position)?.getStampPath(app.applicationContext)
-                if (holder?.imageView != null) {
-                    Glide.with(app.applicationContext).load(Uri.parse(path))
-                            .into(holder.imageView)
-                }
-                holder?.view?.setOnClickListener({ itemClickListener?.onClick(stamp) })
-            }
-        }
-    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+            holder.bind(app.applicationContext, list!![position], itemClickListener!!)
+
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val view = itemView
         val imageView = itemView.iv_stamp!!
 
+        fun bind(context: Context, stamp: Stamp, itemClickListener: OnItemClickListener) {
+            if (stamp.isGathered) {
+                val path = stamp.getStampPath(context)
+                Glide.with(context).load(Uri.parse(path))
+                        .into(imageView)
+                view.setOnClickListener({ itemClickListener?.onClick(stamp) })
+            }
+        }
+
+        fun recycle(context: Context) {
+            Glide.with(context).clear(imageView)
+        }
     }
 
     interface OnItemClickListener {
